5db8155160771e1c1bf369c2d24f42a02e15c000
2ssue <e2ssue@gmail.com>
2019-09-17 19:22:08 +0900

    Update Feature: 버튼을 눌렀을 때 각 방향으로 슬라이딩되도록 하는 기능 추가
    프로젝트 요구사항 중 미니 캐로셀의 2차 기능, 버튼을 눌렀을 때 슬라이드가 되도록 구현
    요구사항에서는 버튼을 누른 후 일정 시간이 지나면 다시 정방향으로 가도록 했는데, setTimeout으로 구현 시 이벤트가 중첩되어 걸리는 현상이 있어 일단 기존과 동일하게 setInterval으로 설정해주었음

--- javascripts/carousel.js
+++ javascripts/carousel.js
@@ -31,7 +31,51 @@ function slideCardForward(){
     }
 }
 
+function slideCardReverse(){
+    const lastCard = document.querySelector(`#${this.id} .card__item:last-child`);
+    let currentCard = document.querySelector(`.${SHOWING_CLASS}`);
+    let previousCard = currentCard.previousElementSibling;
+
+    const enrollPreviousCard = (currentCard) => {
+        const previousCard = currentCard.previousElementSibling;
+        previousCard.classList.add(UNSHOWING_CLASS);
+    }
+
+    if(currentCard){
+        if(previousCard){
+            currentCard.classList.remove(SHOWING_CLASS);
+            previousCard.classList.remove(UNSHOWING_CLASS);
+            previousCard.classList.add(SHOWING_CLASS);
+            
+            if(previousCard.previousElementSibling){
+                enrollPreviousCard(previousCard);
+            }else{
+                lastCard.classList.add(UNSHOWING_CLASS);
+            }
+        }else{
+            lastCard.classList.remove(UNSHOWING_CLASS);
+            lastCard.classList.add(SHOWING_CLASS);
+            currentCard.classList.remove(SHOWING_CLASS);
+            enrollPreviousCard(lastCard);
+        }
+    }
+}
+
 const miniCarouselContainer = document.getElementById('container');
 let intervalSliding = setInterval(slideCardForward.bind(miniCarouselContainer), 3000);
 
 slideCardForward.bind(miniCarouselContainer)();
+
+const rightButton = document.getElementById('button__slide-right');
+rightButton.addEventListener('click', () => {
+    clearInterval(intervalSliding);
+    slideCardForward.bind(miniCarouselContainer)();
+    intervalSliding = setInterval(slideCardForward.bind(miniCarouselContainer), 3000);
+});
+
+const leftButton = document.getElementById('button__slide-left');
+leftButton.addEventListener('click', () => {
+    clearInterval(intervalSliding);
+    slideCardReverse.bind(miniCarouselContainer)();
+    intervalSliding = setInterval(slideCardForward.bind(miniCarouselContainer), 3000);
+})
