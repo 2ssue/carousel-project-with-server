d86203eed610984dfce4abae96bae9fc1e6b598d
2ssue <e2ssue@gmail.com>
2019-09-18 13:54:45 +0900

    Update Feature: 캐로셀 슬라이드 이벤트 클래스로 분리
    이후 하단 캐로셀을 구현시, 두 개의 동작 방식이 비슷하므로 이를 활용하기 위해 클래스로 분리하였음

--- javascripts/carousel.js
+++ javascripts/carousel.js
@@ -1,81 +1,72 @@
 const SHOWING_CLASS = 'showing';
 const UNSHOWING_CLASS = 'unshowing';
 
-function slideCardForward(){
-    const firstCard = document.querySelector(`#${this.id} .card__item:first-child`);
-    const currentCard = document.querySelector(`.${SHOWING_CLASS}`);
+export default class Carousel{
+    constructor(slidingContainer){
+        this.container = slidingContainer;
+        this.firstCard = slidingContainer.querySelector('.card__item:first-child');
+        this.lastCard = slidingContainer.querySelector('.card__item:last-child');
+    }
+
+    setStartToEndCard(){
+        this.firstCard = this.container.querySelector('.card__item:first-child');
+        this.lastCard = this.container.querySelector('.card__item:first-child');
+    }
+
+    slideCardForward(){
+        const currentCard = this.container.querySelector(`.${SHOWING_CLASS}`);
+
+        const enrollPreviousCard = () => {
+            this.firstCard.classList.add(SHOWING_CLASS);
+            this.lastCard.classList.add(UNSHOWING_CLASS);
+        }
     
-    const enrollPreviousCard = () => {
-        firstCard.classList.add(SHOWING_CLASS);
-        const lastCard = document.querySelector(`#${this.id} .card__item:last-child`);
-        lastCard.classList.add(UNSHOWING_CLASS);
-    }
-
-    if(currentCard){
-        currentCard.classList.remove(SHOWING_CLASS);
-        const nextCard = currentCard.nextElementSibling;
-        const previousCard = document.querySelector(`.${UNSHOWING_CLASS}`);
-
-        if(previousCard){
-            previousCard.classList.remove(UNSHOWING_CLASS);
-        }
-        if(nextCard){
-            nextCard.classList.add(SHOWING_CLASS);
-            currentCard.classList.add(UNSHOWING_CLASS);
-        }else{
-            enrollPreviousCard();
-            currentCard.classList.add(UNSHOWING_CLASS);
-        }
-    }else{
-        enrollPreviousCard();
-    }
-}
-
-function slideCardReverse(){
-    const lastCard = document.querySelector(`#${this.id} .card__item:last-child`);
-    let currentCard = document.querySelector(`.${SHOWING_CLASS}`);
-    let previousCard = currentCard.previousElementSibling;
-
-    const enrollPreviousCard = (currentCard) => {
-        const previousCard = currentCard.previousElementSibling;
-        previousCard.classList.add(UNSHOWING_CLASS);
-    }
-
-    if(currentCard){
-        if(previousCard){
+        if(currentCard){
             currentCard.classList.remove(SHOWING_CLASS);
-            previousCard.classList.remove(UNSHOWING_CLASS);
-            previousCard.classList.add(SHOWING_CLASS);
-            
-            if(previousCard.previousElementSibling){
-                enrollPreviousCard(previousCard);
+            const nextCard = currentCard.nextElementSibling;
+            const previousCard = this.container.querySelector(`.${UNSHOWING_CLASS}`);
+    
+            if(previousCard){
+                previousCard.classList.remove(UNSHOWING_CLASS);
+            }
+            if(nextCard){
+                nextCard.classList.add(SHOWING_CLASS);
+                currentCard.classList.add(UNSHOWING_CLASS);
             }else{
-                lastCard.classList.add(UNSHOWING_CLASS);
+                enrollPreviousCard();
+                currentCard.classList.add(UNSHOWING_CLASS);
             }
         }else{
-            lastCard.classList.remove(UNSHOWING_CLASS);
-            lastCard.classList.add(SHOWING_CLASS);
-            currentCard.classList.remove(SHOWING_CLASS);
-            enrollPreviousCard(lastCard);
+            enrollPreviousCard();
+        }
+    }
+
+    slideCardReverse(){
+        let currentCard = this.container.querySelector(`.${SHOWING_CLASS}`);
+        let previousCard = currentCard.previousElementSibling;
+    
+        const enrollPreviousCard = (currentCard) => {
+            const previousCard = currentCard.previousElementSibling;
+            previousCard.classList.add(UNSHOWING_CLASS);
+        }
+    
+        if(currentCard){
+            if(previousCard){
+                currentCard.classList.remove(SHOWING_CLASS);
+                previousCard.classList.remove(UNSHOWING_CLASS);
+                previousCard.classList.add(SHOWING_CLASS);
+                
+                if(previousCard.previousElementSibling){
+                    enrollPreviousCard(previousCard);
+                }else{
+                    this.lastCard.classList.add(UNSHOWING_CLASS);
+                }
+            }else{
+                this.lastCard.classList.remove(UNSHOWING_CLASS);
+                this.lastCard.classList.add(SHOWING_CLASS);
+                currentCard.classList.remove(SHOWING_CLASS);
+                enrollPreviousCard(this.lastCard);
+            }
         }
     }
 }
-
-const miniCarouselContainer = document.getElementById('container');
-let intervalSliding = setInterval(slideCardForward.bind(miniCarouselContainer), 3000);
-
-slideCardForward.bind(miniCarouselContainer)();
-
-const rightButton = document.getElementById('button__slide-right');
-rightButton.addEventListener('click', () => {
-    clearInterval(intervalSliding);
-    slideCardForward.bind(miniCarouselContainer)();
-    intervalSliding = setInterval(slideCardForward.bind(miniCarouselContainer), 3000);
-});
-
-const leftButton = document.getElementById('button__slide-left');
-leftButton.addEventListener('click', () => {
-    clearInterval(intervalSliding);
-    slideCardReverse.bind(miniCarouselContainer)();
-    intervalSliding = setInterval(slideCardForward.bind(miniCarouselContainer), 3000);
-})
