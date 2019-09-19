1c20c1690d1d0c256a12c1e6df655ef164d6c3aa
2ssue <e2ssue@gmail.com>
2019-09-19 19:55:57 +0900

    Refactoring: 다른 함수에서도 함수들을 호출 가능하도록 구조 변경
    캐로셀 이동 시에도 카드가 커지는 이벤트가 있기 때문에, 이벤트 기반 동작 방식에서 파라미터 방식으로 변경함
    그 외 캐로셀 버튼을 선택할 때도 동일한 이유로 구조 변경

--- javascripts/card.js
+++ javascripts/card.js
@@ -1,6 +1,11 @@
 const CARDITEM_CLASS = 'card__item';
 const SELECTED_CLASS = 'selected';
 const CAROUSEL_BUTTON_CLASS = 'carousel-page-button';
+const CAROUSEL_BUTTON_CONTAINER_CLASS = 'carousel-button-container'
+const CAROUSELITEM_CLASS = 'carousel__item';
+const CAROUSEL_CLASS = 'carousel__container';
+const PREV_CAROUSEL_BUTTON = 'button-prev__long';
+const NEXT_CAROUSEL_BUTTON = 'button-next__long';
 
 export default class Card{
     constructor(cardContainer, carouselList){
@@ -8,36 +13,48 @@ export default class Card{
         this.carouselList = carouselList;
     }
 
-    makeCardBigger(event){
-        if(event.target === this.container) return;
-        
+    cardClickEventHandler(event){
+        if(event.target === this.container || event.target === this.container.firstElementChild) return;
+
         let parent = event.target.parentElement;
 
-        if(event.target.tagName === 'SPAN')
-            parent = event.target.parentElement.parentElement;
-        
-        const selectedItem = this.container.querySelector(`.${CARDITEM_CLASS}.${SELECTED_CLASS}`);
-        
-        this.removeSelectedCarouselButton();
-        this.unshowCarouselButtons();
-
-        if(selectedItem)
-            selectedItem.classList.remove(SELECTED_CLASS);
-
+        if(event.target.tagName === 'SPAN') return;
+            
         if(parent.id){
-            event.target.classList.add(SELECTED_CLASS);
+            this.makeCardBigger(event.target);
         }else{
-            parent.classList.add(SELECTED_CLASS);
+            this.makeCardBigger(parent);
+        }
+    }
+
+    makeCardBigger(card, nonecarouselEvent){
+        if(card){
+            const selectedItem = this.container.querySelector(`.${CARDITEM_CLASS}.${SELECTED_CLASS}`);
+
+            if(selectedItem)
+                selectedItem.classList.remove(SELECTED_CLASS);
+
+            this.removeSelectedCarouselButton();
+            this.unshowCarouselButtons();
+        }else{
+            card = this.container.querySelector(`.${CARDITEM_CLASS}`);
         }
 
-        this.showCarouselButtons(event);
-        this.selectFirstCarouselButton(event);
+        card.classList.add(SELECTED_CLASS);
+        this.showCarouselButtons();
+        if(nonecarouselEvent) return;
+        this.selectFirstCarouselButton();
+        this.showNthCarousel(this.getElementIndex(card), 0);
     }
 
     selectCarouselButton(event){
         if(event.target.tagName === 'SPAN'){
             this.removeSelectedCarouselButton();
-            event.target.classList.add(SELECTED_CLASS);    
+            event.target.classList.add(SELECTED_CLASS);
+
+            const cardIndex = this.getElementIndex(event.target.parentElement.parentElement);
+            const carouselIndex = this.getElementIndex(event.target);
+            this.showNthCarousel(cardIndex, carouselIndex);
         }
     }
 
@@ -48,32 +65,23 @@ export default class Card{
             selectedItem.classList.remove(SELECTED_CLASS);
     }
 
-    selectFirstCarouselButton(event){
-        const parent = event.target.parentElement;
-
-        let firstCarouselButton;
-        if(parent.id){
-            firstCarouselButton = event.target.querySelector(`.${CAROUSEL_BUTTON_CLASS}`);
-        }else{
-            firstCarouselButton = parent.querySelector(`.${CAROUSEL_BUTTON_CLASS}`);
+    selectFirstCarouselButton(){
+        const selectedCard = this.container.querySelector(`.${CARDITEM_CLASS}.${SELECTED_CLASS}`);
+        
+        if(selectedCard){
+            selectedCard.querySelector(`.${CAROUSEL_BUTTON_CLASS}`).classList.add(SELECTED_CLASS);
         }
-
-        firstCarouselButton.classList.add(SELECTED_CLASS);
     }
 
-    showCarouselButtons(event){
-        const parent = event.target.parentElement;
+    showCarouselButtons(){
+        const selectedCard = this.container.querySelector(`.${CARDITEM_CLASS}.${SELECTED_CLASS}`);
 
-        let carouselButtons;
-        if(parent.id){
-            carouselButtons = event.target.querySelectorAll(`.${CAROUSEL_BUTTON_CLASS}`);
-        }else{
-            carouselButtons = parent.querySelectorAll(`.${CAROUSEL_BUTTON_CLASS}`);
+        if(selectedCard){
+            const carouselButtons = selectedCard.querySelectorAll(`.${CAROUSEL_BUTTON_CLASS}`);
+            carouselButtons.forEach((button) => {
+                button.classList.add('show');
+            })
         }
-
-        carouselButtons.forEach((button) => {
-            button.classList.add('show');
-        })
     }
 
     unshowCarouselButtons(){
