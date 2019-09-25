9373b151fff2304de2a77ad93ef1ad9c75921c60
2ssue <e2ssue@gmail.com>
2019-09-25 18:59:22 +0900

    Update Feature: 이미지 서버로 업로드 하는 기능 구현

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const DatabaseManager = require('../components/db.js');
 const Templator = require('../components/template.js');
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'public/images/');
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+});
+const upload = multer({storage: storage});
+
+const script = (message, redirect) => {
+    let script = `<script type='text/javascript'>alert('${message}');`;
+    if(redirect) script += `location.href='${redirect}'`;
+    script += '</script>';
+    return script;
+}
 
 router.get('/get/users', function(req, res, next){
     const userDB = new DatabaseManager({
@@ -22,11 +40,8 @@ router.get('/get/users', function(req, res, next){
 });
 
 router.get('/', function(req, res, next){
-    const script = (message) => {
-        return `<script type='text/javascript'>alert('${message}'); location.href = '/';</script>`;
-    }
     if(!req.user){
-        res.send(script('로그인이 필요합니다'));
+        res.send(script('로그인이 필요합니다', '/'));
     }else if(req.user.auth > 0){
       res.render('admin', {
         title: 'ADMIN',
@@ -35,8 +50,12 @@ router.get('/', function(req, res, next){
         linktext: '로그아웃'
       });
     }else if(req.user){
-        res.send(script('관리자가 아닙니다'));
+        res.send(script('관리자가 아닙니다', '/'));
     }
   });
+
+router.post('/upload', upload.single('imagefile'), function(req, res){
+    res.send(script('이미지가 성공적으로 업로드 되었습니다', '/admin'));
+});
 
 module.exports = router;
